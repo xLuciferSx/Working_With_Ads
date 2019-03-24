ba8589851e655a3c43f78d666f8ae9cab2740ca0
Kilo Loco <kiloloco@me.com>
2019-03-24 08:41:20 -0700

    starter

--- Working With Ads/ViewController.swift
+++ Working With Ads/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
