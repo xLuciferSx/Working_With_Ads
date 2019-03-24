ff9ae91538bbb588d0276d14a116520b5a27ddbf
Kilo Loco <kiloloco@me.com>
2019-03-24 07:56:54 -0700

    Initial Commit

---
+++ Working With Ads/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Working With Ads
+//
+//  Created by Kyle Lee on 3/24/19.
+//  Copyright © 2019 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
