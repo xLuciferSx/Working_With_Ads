0ddbc32612e2371a76922b8e68481316f2ad59b9
Raivis <raivis.olehno@applikation.co.uk>
2020-07-17 14:12:06 +0100

    Ad will reload after opening and closing.

--- Working With Ads/ViewController.swift
+++ Working With Ads/ViewController.swift
@@ -16,23 +16,35 @@ class ViewController: UIViewController {
 
     override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
     @IBOutlet weak var bannerView: GADBannerView!
-    var interstial: GADInterstitial!
+    var interstitial: GADInterstitial!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        interstial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
         bannerView.delegate = self
+        interstitial = createAndLoadInterstitial()
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
         let request = GADRequest()
-        interstial.load(request)
+        interstitial.load(request)
     }
     
+
+    func createAndLoadInterstitial() -> GADInterstitial {
+      var interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+      interstitial.delegate = self
+      interstitial.load(GADRequest())
+      return interstitial
+    }
+
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+      interstitial = createAndLoadInterstitial()
+    }
     
     @IBAction func interstialShowAd(_ sender: Any) {
-        if interstial.isReady {
-            interstial.present(fromRootViewController: self)
+        if interstitial.isReady {
+            interstitial.present(fromRootViewController: self)
         } else {
             print("Ad wasn't ready")
         }
@@ -72,6 +84,39 @@ extension ViewController: GADBannerViewDelegate {
     func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
       print("adViewWillLeaveApplication")
     }
-    
 }
+    
+    extension ViewController: GADInterstitialDelegate {
+        /// Tells the delegate an ad request succeeded.
+        func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+          print("interstitialDidReceiveAd")
+        }
+
+        /// Tells the delegate an ad request failed.
+        func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
+          print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
+        }
+
+        /// Tells the delegate that an interstitial will be presented.
+        func interstitialWillPresentScreen(_ ad: GADInterstitial) {
+          print("interstitialWillPresentScreen")
+        }
+
+        /// Tells the delegate the interstitial is to be animated off the screen.
+        func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+          print("interstitialWillDismissScreen")
+        }
+
+//        /// Tells the delegate the interstitial had been animated off the screen.
+//        func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+//          print("interstitialDidDismissScreen")
+//        }
+
+        /// Tells the delegate that a user click will open another app
+        /// (such as the App Store), backgrounding the current app.
+        func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
+          print("interstitialWillLeaveApplication")
+        }
+    }
+
 
